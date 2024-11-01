9fc4c3d34ddb5f198cce612a8f3c296b1c0078ef
typicalsleepingboy <mpluto772@gmail.com>
2024-11-01 14:36:02 +0700

    Fix

--- utils/fetchYouTubeData.js
+++ utils/fetchYouTubeData.js
@@ -3,10 +3,18 @@ const axios = require('axios');
 const fetchYouTubeData = async () => {
   try {
     const response = await axios.get('https://api.crstlnz.my.id/api/jkt48_youtube');
-    return response.data;
+    return {
+      status: 200,
+      data: response.data,
+    };
   } catch (error) {
     console.error('Error fetching data from API:', error);
-    throw error;
+    
+    return {
+      status: 500,
+      message: 'Internal Server Error',
+      error: error.message || 'An unknown error occurred.',
+    };
   }
 };
 
